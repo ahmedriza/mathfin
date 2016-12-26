919ef1c8ec17aaa61a1f3595e07bed18cd1e10d8
onedigit <dr.ahmed_riza@yahoo.co.uk>
2016-12-26 15:42:53 +0000

    add licensing information

--- time/businessdayconvention.cpp
+++ time/businessdayconvention.cpp
@@ -1,3 +1,43 @@
+/*
+  Copyright (C) 2016 Ahmed Riza
+
+  This file is part of MathFin.
+
+  This program is free software: you  can redistribute it and/or modify it
+  under the  terms of the GNU  General Public License as  published by the
+  Free Software Foundation,  either version 3 of the License,  or (at your
+  option) any later version.
+
+  This  program  is distributed  in  the  hope  that  it will  be  useful,
+  but  WITHOUT  ANY  WARRANTY;  without   even  the  implied  warranty  of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+  Public License for more details.
+
+  You should have received a copy  of the GNU General Public License along
+  with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/*
+  Copyright (C) 2000, 2001, 2002, 2003 RiskMap srl
+  Copyright (C) 2003, 2004, 2005, 2006, 2007 StatPro Italia srl
+  Copyright (C) 2006 Piter Dias
+  Copyright (C) 2014 Paolo Mazzocchi
+
+  This file is part of QuantLib, a free-software/open-source library
+  for financial quantitative analysts and developers - http://quantlib.org/
+
+  QuantLib is free software: you can redistribute it and/or modify it
+  under the terms of the QuantLib license.  You should have received a
+  copy of the license along with this program; if not, please email
+  <quantlib-dev@lists.sf.net>. The license is also available online at
+  <http://quantlib.org/license.shtml>.
+
+  This program is distributed in the hope that it will be useful, but WITHOUT
+  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+  FOR A PARTICULAR PURPOSE.  See the license for more details.
+
+*/
+
 #include <time/businessdayconvention.h>
 #include <base/error.h>
 #include <iostream>
--- time/businessdayconvention.h
+++ time/businessdayconvention.h
@@ -1,7 +1,22 @@
-#ifndef FINMATH_BUSINESS_DAY_CONVENTION_H
-#define FINMATH_BUSINESS_DAY_CONVENTION_H
+/*
+  Copyright (C) 2016 Ahmed Riza
 
-#include <iosfwd>
+  This file is part of MathFin.
+
+  This program is free software: you  can redistribute it and/or modify it
+  under the  terms of the GNU  General Public License as  published by the
+  Free Software Foundation,  either version 3 of the License,  or (at your
+  option) any later version.
+
+  This  program  is distributed  in  the  hope  that  it will  be  useful,
+  but  WITHOUT  ANY  WARRANTY;  without   even  the  implied  warranty  of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+  Public License for more details.
+
+  You should have received a copy  of the GNU General Public License along
+  with this program. If not, see <http://www.gnu.org/licenses/>.
+
+*/
 
 /*
   Copyright (C) 2000, 2001, 2002, 2003 RiskMap srl
@@ -21,8 +36,13 @@
   This program is distributed in the hope that it will be useful, but WITHOUT
   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
   FOR A PARTICULAR PURPOSE.  See the license for more details.
+
 */
 
+#ifndef FINMATH_BUSINESS_DAY_CONVENTION_H
+#define FINMATH_BUSINESS_DAY_CONVENTION_H
+
+#include <iosfwd>
 
 namespace FinMath {
 
